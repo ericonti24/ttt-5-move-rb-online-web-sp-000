59371e1d6992298f23893e32120c2389e1a5653a
Eric Ontiveros <ericontiveros123@gmail.com>
2021-04-12 20:03:45 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,7 +11,7 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, character
+def move(board, index, character)
   board[index] = character
   character = "X"
 end
